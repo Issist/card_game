8d986390ccdc6a51742d0d842034794dd89f474a
Issist <120555920+Issist@users.noreply.github.com>
2023-02-22 20:29:56 +0500

    finale

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+//RANGESLIDER
+var slider = document.getElementById("slider");
+var valueDisplay = document.getElementById("valueDisplay");
+
+slider.addEventListener("input", function() {
+  valueDisplay.innerHTML = slider.value;
+});
+
+
 // Select all divs with an id
 var cards = document.querySelectorAll("div[id^='card']");
 
 // Convert the NodeList to an array
 var deck = Array.from(cards);
-
-//difficulty lvl
-
 
 
 
@@ -15,68 +21,82 @@ function shuffleDeck(deck) {
         let j = Math.floor(Math.random() * (i + 1));
         [deck[i], deck[j]] = [deck[j], deck[i]];
     }
-    return deck;
+    return deck;    
 }
 
-// Shuffle the div elements
-var shuffledDeck = shuffleDeck(deck);
-console.log(shuffledDeck);
+//START GAME
+let btn_start = document.querySelector('.btn_start');
+let btn_next = document.querySelector('.btn_next');
 
-// Display the shuffled div elements one by one
-var currentCard = 0;
+btn_start.addEventListener('click', () => {
+    // Shuffle the div elements
+    var shuffledDeck = shuffleDeck(deck).slice(52-slider.value);
+    console.log(shuffledDeck);
+    // Display the shuffled div elements one by one
+    var currentCard = 0;
 
- // Hide all the divs
-function displayCard(){
-    for (let i = 0; i < shuffledDeck.length; i++){
-        shuffledDeck[i].style.display = 'none';
-    }
-    
-    if (currentCard >= shuffledDeck.length){
+    //Hide start button show next button
+    btn_start.style.display = 'none';
+    btn_next.style.display = 'flex';
+
+    // Hide all the divs
+    function displayCard(){
         for (let i = 0; i < shuffledDeck.length; i++){
-            shuffledDeck[i].style.display = 'block ';
-            shuffledDeck[i].style.maxWidth = '150px';
+            shuffledDeck[i].style.display = 'none';
         }
-    } else {
-        // Show the current div
-        shuffledDeck[currentCard].style.display = 'block';
-        currentCard++;
-    }
-}
-
-//Button next card
-let button = document.querySelector('.btn');
-button.addEventListener('click', () => {
-    displayCard();
-    button.innerHTML = 'next';
-});
-
-//errors number
-let errorsScore = 0;
-const errors_span = document.getElementById('errorsNumber');
-
-//card selection
-let currentIndex = 0;
-let title = document.querySelector('h1');
-
-shuffledDeck.forEach(function(card) {
-    card.addEventListener('click', function(){
-        if (this === shuffledDeck[currentIndex]){
-            currentIndex++;          
-            this.classList.add('green');
-            this.classList.remove('red');
-            this.style.pointerEvents = "none";
-            title.textContent = "Let's check your memory";
-            button.innerHTML = 'play'
-            if(currentIndex >= shuffledDeck.length){
-                title.textContent = "Done!";
+        
+        if (currentCard >= shuffledDeck.length){
+            for (let i = 0; i < shuffledDeck.length; i++){
+                shuffledDeck[i].style.display = 'block ';
+                shuffledDeck[i].style.maxWidth = '150px';
+                btn_next.style.display = 'none';
             }
-        }else{
-            this.classList.add('red');
-            errorsScore++;
-            errors_span.innerHTML = errorsScore;
+        } else {
+            // Show the current div
+            shuffledDeck[currentCard].style.display = 'block';
+            currentCard++;
         }
-    })
+    }
+    displayCard();
+    //Button next card
+    //let button = document.querySelector('.btn');
+    btn_next.addEventListener('click', () => {
+        displayCard();
+    });
+
+    //errors number
+    let errorsScore = 0;
+    const errors_span = document.getElementById('errorsNumber');
+
+    //card selection
+    let currentIndex = 0;
+    let title = document.querySelector('h1');
+
+    shuffledDeck.forEach(function(card) {
+        card.addEventListener('click', function(){
+            if (this === shuffledDeck[currentIndex]){
+                currentIndex++;          
+                this.classList.add('green');
+                this.classList.remove('red');
+                this.style.pointerEvents = "none";
+                title.textContent = "Let's check your memory";
+                if(currentIndex >= shuffledDeck.length){
+                    title.textContent = "Done! Play again after 3s :)";
+                    setTimeout(() => {
+                      location.reload();  
+                    }, 3000);
+                    
+                }
+            }else{
+                this.classList.add('red');
+                errorsScore++;
+                errors_span.innerHTML = errorsScore;
+            }
+        })
+    });
 });
+
+
 
 
 
